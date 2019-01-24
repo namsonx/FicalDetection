148ea051a69759eda85ce32a01d981efcdb9d3d9
MAS2HC <MAS2HC@HC-UT40281N.hc.apac.bosch.com>
2019-01-24 10:15:44 +0700

    making 1 file for python 3

--- ficialRecognition3.py
+++ ficialRecognition3.py
@@ -40,14 +40,14 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
             image = face_recognition.load_image_file(img_path)
             face_bounding_boxes = face_recognition.face_locations(image)   
             if len(face_bounding_boxes) != 1:
-                print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+                print(("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face")))
             else:
                 X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
                 y.append(class_dir)
     # Determine how many neighbors to use for weighting in the KNN classifier
     if n_neighbors is None:
         n_neighbors = int(round(math.sqrt(len(X))))
-        print("Chose n_neighbors automatically:", n_neighbors)
+        print(("Chose n_neighbors automatically:", n_neighbors))
     knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
     knn_clf.fit(X, y)
     if model_save_path is not None:
@@ -59,7 +59,7 @@ def create_model(train_dir, model_path):
     print("Training KNN classifier...")
     classifier = train(train_dir, model_save_path = model_path, n_neighbors=2)
     print("Training complete!")
-    print type(classifier)
+    print(type(classifier))
     return classifier
 
 def save_image(name):
@@ -70,7 +70,7 @@ def save_image(name):
     try:
         while True:
             sleep(0.2)
-            print "Started capture pics. Press key Ctrl+c to start build train model"
+            print("Started capture pics. Press key Ctrl+c to start build train model")
             pic = cv2.VideoCapture(cam_link)
             ret, frame = pic.read()
             rgb_frame = frame[:, :, ::-1]
@@ -79,15 +79,15 @@ def save_image(name):
                 continue
             img_path = img_folder + "/" + name + "_" + str(index) + ".jpg"
             status = cv2.imwrite(img_path, frame)
-            print "save image number %s status: %s." %(index, status)
+            print("save image number %s status: %s." %(index, status))
             index = index + 1
     except KeyboardInterrupt:
-        print 'Ended collecting pics'
+        print('Ended collecting pics')
         pass
         
 
 def main():
-    print 'starting main function'        
+    print('starting main function')        
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--train', help='Running train mode, name of person who is going to train have to input')
     args = parser.parse_args()
@@ -99,7 +99,7 @@ def main():
     if args.train==None:
         try:
             while True:
-                print "Loop running"
+                print("Loop running")
                 person = {}
                 video_capture = cv2.VideoCapture(cam_link)
                 distance_threshold = 0.45
@@ -115,7 +115,7 @@ def main():
                 are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(face_locations))]
                 for pred, loc, rec in zip(knn_clf.predict(faces_encodings), face_locations, are_matches):
                     if rec:
-                        print pred, loc
+                        print(pred, loc)
                         check = 0
                         for p in vistor:
                             if p['name']==pred:
@@ -134,9 +134,9 @@ def main():
                             client.connect(broker, port)
                             client.publish("test/detection", display_name)
                     else:
-                        print "unkown", loc
+                        print("unkown", loc)
         except KeyboardInterrupt:
-            print 'Stoped face detection application'
+            print('Stoped face detection application')
             pass
 
 if __name__ == "__main__":
